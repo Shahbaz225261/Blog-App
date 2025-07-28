5f029d6049a3157e5d55b43588528ea531979632
shahabz <roxxshahbaz781@gmail.com>
2025-07-28 21:53:04 +0530

    authoptions added

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,9 +1,10 @@
 import Signin from "@/components/Signin";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
+import { authOptions } from "../api/auth/[...nextauth]/route";
 
 export default async function SignInPage (){
-  const session = await getServerSession();
+  const session = await getServerSession(authOptions);
   if(session){
     redirect('/blogs')
   }
