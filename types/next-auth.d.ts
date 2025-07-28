9d3ffc8f281acad41c5a078ed63e2ccc61e088fc
shahabz <roxxshahbaz781@gmail.com>
2025-07-28 21:04:40 +0530

    added custom types for jwt and session

---
+++ types/next-auth.d.ts
@@ -0,0 +1,15 @@
+import NextAuth, { DefaultSession } from "next-auth";
+
+declare module "next-auth" {
+  interface User {
+    id?: string;
+    username?: string;
+  }
+
+  interface Session {
+    user: {
+      id?: string;
+      username?: string;
+    } & DefaultSession["user"];
+  }
+}
